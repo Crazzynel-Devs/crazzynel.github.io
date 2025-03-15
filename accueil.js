125d3b681e5112a6aac8017c88460abd2752925f
westekey <146666994+westekey@users.noreply.github.com>
2025-03-15 14:50:04 +0000

    cestdejaca

--- accueil.js
+++ accueil.js
@@ -114,3 +114,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector("#navLinks");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
